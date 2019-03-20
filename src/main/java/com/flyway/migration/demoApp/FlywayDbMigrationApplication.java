db271bfb652f6c31e868c715b13418bd18767d17
e068635 <elson.franca@mastercard.com>
2019-03-20 18:28:14 -0300

    initial version

---
+++ src/main/java/com/flyway/migration/demoApp/FlywayDbMigrationApplication.java
@@ -0,0 +1,14 @@
+package com.flyway.migration.demoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class FlywayDbMigrationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayDbMigrationApplication.class, args);
+    }
+
+}
+
